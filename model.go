053968d733b0fcceeb75930ab0ec6c92e41dcac1
armmydev <arm@shopseason.co>
2016-08-19 02:45:44 +0700

    Change JSON name response on gorm.Model

--- model.go
+++ model.go
@@ -7,8 +7,8 @@ import "time"
 //      gorm.Model
 //    }
 type Model struct {
-	ID        uint `gorm:"primary_key"`
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt *time.Time `sql:"index"`
+	ID        uint       `gorm:"primary_key" json:"Id"`
+	CreatedAt time.Time  `json:"-"`
+	UpdatedAt time.Time  `json:"-"`
+	DeletedAt *time.Time `sql:"index" json:"-"`
 }
